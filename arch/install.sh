2c67640bfae0a11851c0b681c14f0a4fc25ac008
mark182182 <versalife11@hotmail.com>
2019-06-21 17:42:38 +0200

    move(*): to arch folder

--- arch/install.sh
+++ arch/install.sh
@@ -4,8 +4,10 @@ echo 'Installing packages...'
 
 sudo pacman -Syyuu -y
 sudo pacman -S emacs -y
-echo 'Spacemacs'
+sudo pacman -S git -y
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 sudo pacman -S vim -y
 sudo pacman -S opera -y
 sudo pacman -S google-chrome-stable -y
+sudo pacman -S dunst
+cp -r ./.* ~/
