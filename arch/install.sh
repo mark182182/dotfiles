7da91dc36c5fd3c27077f1950533c988cf485506
mark182182 <versalife11@hotmail.com>
2019-06-22 14:06:17 +0200

    add xorg and dmenu

--- arch/install.sh
+++ arch/install.sh
@@ -7,11 +7,14 @@ sudo pacman -S emacs --noconfirm
 sudo pacman -S fish --noconfirm
 sudo pacman -S opera --noconfirm
 sudo pacman -S dunst --noconfirm
-sudo pacman -S dwm --noconfirm
 sudo pacman -S thunar --noconfirm
 sudo pacman -S mc --noconfirm
 sudo pacman -S refind-efi --noconfirm
 sudo pacman -S dialog --noconfirm
+sudo pacman -S xorg --noconfirm
+sudo pacman -S dmenu
+sudo pacman -S xorg-xinit --noconfirm
+sudo pacman -S wpa_supplicant --noconfirm
 
 echo 'Installing from github...'
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
@@ -20,8 +23,11 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
+echo 'Install window manager...'
+yay -S dwm
+
 echo 'Copying dotfiles...'
 cp -r ./.* /home/$USERNAME/
 
-echo 'Changing shell'
+echo 'Changing shell...'
 sudo chsh -s /usr/bin/fish $USERNAME
