a33ef30df5d691ff52f6a059324f59f1f80cb3a2
mark182182 <versalife11@hotmail.com>
2019-06-22 15:27:45 +0200

    add packages

--- arch/install.sh
+++ arch/install.sh
@@ -6,16 +6,19 @@ sudo pacman -Syyuu --noconfirm
 sudo pacman -S emacs --noconfirm
 sudo pacman -S fish --noconfirm
 sudo pacman -S opera --noconfirm
+sudo pacman -S opera-ffmpeg-codecs --noconfirm
 sudo pacman -S dunst --noconfirm
 sudo pacman -S thunar --noconfirm
 sudo pacman -S mc --noconfirm
 sudo pacman -S refind-efi --noconfirm
 sudo pacman -S dialog --noconfirm
 sudo pacman -S xorg --noconfirm
-sudo pacman -S dmenu
+sudo pacman -S dmenu --noconfirm
 sudo pacman -S xorg-xinit --noconfirm
 sudo pacman -S wpa_supplicant --noconfirm
 sudo pacman -S alsa-utils --noconfirm
+sudo pacman -S termite --noconfirm
+sudo pacman -S openssh --noconfirm
 
 echo 'Installing from github...'
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
