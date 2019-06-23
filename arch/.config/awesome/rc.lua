5e0ce2176cef8218292005bc748ca73c01679f51
mark182182 <versalife11@hotmail.com>
2019-06-23 15:55:45 +0200

    remove mod + x

--- arch/.config/awesome/rc.lua
+++ arch/.config/awesome/rc.lua
@@ -342,16 +342,6 @@ globalkeys = gears.table.join(
     awful.key({modkey}, "l", function () awful.spawn.with_shell("/home/vsl/rickit.sh")
     end, {description = "lock the screen", group = "launcher"}),
 
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
     awful.key({ modkey }, "p", function() awful.spawn.with_shell("lighthm") end,
               {description = "show the menubar", group = "launcher"})
